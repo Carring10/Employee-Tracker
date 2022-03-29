c32d9036f0195ea81697f620a87179d1e307b5ca
Carrington <hartis.c99@gmail.com>
2022-03-29 09:06:37 -0400

    added tables and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (id, name)
+VALUES (1, "Customer Service"),
+       (2, "Sales"),
+       (3, "Finance"),
+       (4, "IT");
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (21, "Front Desk Associate", 40000, 1),
+       (22, "Salesperson", 50000, 2),
+       (23, "Accountant", 60000, 3),
+       (24, "Software Engineer", 70000, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (101, "Avery", "Alfred", 21, 5),
+       (102, "Sylvester", "Smith", 22, 6),
+       (103, "Linda", "Lorey", 23, 7),
+       (104, "Ron", "Rodriguez", 24, 8);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
+const table = require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,3 +17,15 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee_db database.`)
 );
+
+db.query("SELECT * FROM department", (err, results) => {
+  err ? console.log(err) : console.log(results);
+});
+
+db.query("SELECT * FROM roles", (err, results) => {
+  err ? console.log(err) : console.log(results);
+});
+
+db.query("SELECT * FROM employee", (err, results) => {
+  err ? console.log(err) : console.log(results);
+});
